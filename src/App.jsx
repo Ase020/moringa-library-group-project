382483bfd4876431b574cce73cfd7ebf6e4d8fc0
felixNyalenda <felixasejunior@gmail.com>
2023-05-01 17:52:38 +0300

    react-router-dom config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home, Layout } from "./components";
 
 function App() {
-   return <>ase</>;
+   const router = createBrowserRouter([
+      {
+         path: "/",
+         element: <Layout />,
+         children: [
+            {
+               path: "/",
+               element: <Home />,
+            },
+         ],
+      },
+   ]);
+   return (
+      <>
+         <RouterProvider router={router} />
+      </>
+   );
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import { default as Home } from "./Home";
+import { default as Navbar } from "./Navbar";
+import { default as Layout } from "./Layout";
+
+export { Home, Navbar, Layout };
