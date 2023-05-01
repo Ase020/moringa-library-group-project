1f5f4754309280df3b09e48c8e22494b69ae8db0
felixNyalenda <felixasejunior@gmail.com>
2023-05-02 02:06:59 +0300

    book page build

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,6 +1,9 @@
 import { useContext } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+
 import { BooksContext } from "../context/books";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const Book = () => {
    const [books] = useContext(BooksContext);
@@ -8,7 +11,33 @@ const Book = () => {
    const bookObj = books[id - 1];
    console.log(bookObj.title, id);
 
-   return <div>Book</div>;
+   return (
+      <div className="bookDetails">
+         <img src={bookObj.image} alt={bookObj.id} className="bookDetail-img" />
+
+         <div className="more-details">
+            <h4>{bookObj.title}</h4>
+
+            <span>{bookObj.author}</span>
+
+            <p>{bookObj.description}</p>
+
+            <span>{bookObj.genre}</span>
+
+            <p>Publisher: {bookObj.publisher}</p>
+
+            <p>Published on: {bookObj.published}</p>
+
+            <p>ISBN: {bookObj.isbn}</p>
+
+            <button className="add-to-shelf">Add to My Shelf</button>
+
+            <Link to="/" className="go-back">
+               <FontAwesomeIcon icon={faArrowLeft} /> Go back
+            </Link>
+         </div>
+      </div>
+   );
 };
 
 export default Book;
--- src/components/BooksCard.jsx
+++ src/components/BooksCard.jsx
@@ -5,6 +5,11 @@ import { Link } from "react-router-dom";
 // eslint-disable-next-line react/prop-types
 const BooksCard = ({ book }) => {
    const { title, image, id } = book;
+
+   // To randomise the book images
+   // const randomNum = Math.floor(Math.random() * 900) + 100; // generate random 3-digit number
+
+   // const newImage = image.slice(0, -3) + randomNum; // remove last 3 letters and add random number
    return (
       <div className="book-card">
          <img src={image} alt={title} className="book-img" />
