715c93930558a1186c0978091b9b2f8ed89942cc
felixNyalenda <felixasejunior@gmail.com>
2023-05-02 15:02:34 +0300

    My shelf build

--- src/components/MyShelf.jsx
+++ src/components/MyShelf.jsx
@@ -1,5 +1,24 @@
-const MyShelf = () => {
-   return <div>MyShelf</div>;
+/* eslint-disable react/prop-types */
+
+import BooksCard from "./BooksCard";
+import shelf from "../../public/shelf.png";
+
+const EmptyShelf = () => {
+   return (
+      <div className="empty-shelf">
+         <h2>No books on this shelf yet.</h2>
+         <img src={shelf} alt="Empty shelf" />
+      </div>
+   );
+};
+
+const MyShelf = ({ shelf }) => {
+   return (
+      <div className="my-shelf">
+         <h4>My Shelf </h4>
+         <div className="books-wrapper">{shelf.length > 0 ? shelf.map((book) => <BooksCard key={book.id} book={book} />) : <EmptyShelf />}</div>
+      </div>
+   );
 };
 
 export default MyShelf;
