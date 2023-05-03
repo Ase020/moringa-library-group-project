af3e65895de7b7f55c052cd44448c7269a3ac98f
felixNyalenda <felixasejunior@gmail.com>
2023-05-03 11:41:39 +0300

    Footer build

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import { Footer, Navbar } from "../components";
 
 const Layout = () => {
    return (
       <div className="App">
          <Navbar />
          <Outlet />
+         <Footer />
       </div>
    );
 };
